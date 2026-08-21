788351a2a16d8d46d1e0301901dc283b6f76e6c3
odavans <150967888+odavans@users.noreply.github.com>
2026-08-21 23:03:54 +0300

    Remove lib32-libappindicator from AUR_PKGS
    Removed 'lib32-libappindicator' from AUR packages list.

--- dms/aur_pkgs.sh
+++ dms/aur_pkgs.sh
@@ -4,7 +4,6 @@ AUR_PKGS=(
     coolercontrol-bin
     heroic-games-launcher-bin
     hypr-kdeconnect-fix-git
-    lib32-libappindicator
     nautilus-open-any-terminal
     papirus-folders
     payload-dumper-go-bin
